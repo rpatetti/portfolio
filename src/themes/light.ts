71153a19db4cf5d3aee2b6fbd7306755404c8e91
rpatetti <patetti.rafael@gmail.com>
2024-06-05 19:51:55 -0300

    Vericar erro

---
+++ src/themes/light.ts
@@ -0,0 +1,9 @@
+const temaLight = {
+  corPrincipal: 'red',
+  corSecundaria: '#949494',
+  corDeFundo: '#fff',
+  corDeFundoBotao: '#4476bf',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaLight
